8ef20ec2f7d40a9c052be63c891de77b6c4f5928
Naina kumari <118041285+Nainakumari7@users.noreply.github.com>
2024-10-15 20:30:47 +0530

    Create basic.c

---
+++ basic.c
@@ -0,0 +1,77 @@
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+
+#include <stdio.h>
+
+int main() {
+    int age = 20;
+    float pie = 3.14;
+    char hash = '#';
+    printf("hello naina\n");
+    printf("hello naina\n");
+    printf("hello naina\n");
+    printf("hello naina");
+    return 0;
+}
+
+
+#include<stdio.h>
+int main(){
+    int age = 20;
+    char star = '*';
+    float pie = 3.14;
+    printf("The value of age is %d\n",age);
+    printf("The star looks like this %c\n",star);
+    printf("The pie is %f\n",pie);
+    return 0;
+}
+
+
+#include<stdio.h>
+int main(){
+    int age;
+    printf("Enter your age:-");
+    scanf("%d",&age);
+    printf("your age is %d",age);
+    return 0;
+}
+
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter your first number:-");
+    scanf("%d",&a);
+
+    printf("Enter your seacond number:-");
+    scanf("%d",&b);
+    
+    int sum = a+b;
+    printf("the sum is %d":-,sum);
+    return 0;
+}
+
+#include<stdio.h>
+int main(){
+    float side;
+    printf("enter the sides od square:-");
+    scanf("%f",&side);
+    printf("the side of square is %f:-",side*side);
+    return 0;
+
+}
+
+
+#include<stdio.h>
+int main(){
+    float radius;
+    printf("Enter the radius of the circle:-");
+    scanf("%f",&radius);
+    
+    printf("the area of the circle is %f",3.14*radius*radius);
+    return 0;
+}
